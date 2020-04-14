7a9e029c1b7d9a786ee95dedffadf32a7b0bc6b5
zulala <54187814+zulala@users.noreply.github.com>
2020-04-15 00:50:44 +0300

    cookie fix & auto resize

--- src/App.js
+++ src/App.js
@@ -15,17 +15,9 @@ export default class App extends React.Component {
           version: "1.0",
           showToast: false,
           message: "",
-          layout:  
-        [{i: 'Key Partners',        x: 0, y: 0, w: 2, h: 8, text: `* Equinix (for data Center facilities)\n* Content Providers`},
-        {i: 'Key Activities',       x: 2, y: 0, w: 2, h: 4, text: `* Plateform Dvpt`},
-        {i: 'Value Propositions',   x: 4, y: 0, w: 2, h: 8, text: `* Manage Professionnal identity\n* Build Professionnal network\n* Identify & Reach the Right Talent\n* Reach the target Audience\n* Access to LinkedIn data base content via API's widgets`},
-        {i: 'Customer Relations',   x: 6, y: 0, w: 2, h: 4, text: `* Internet Users\n* * Recruiters\n* * Advertisers & Marketers\n* Developers\n* Free offerings & Premium Subscriptions\n* Hiring Solutions\n* Marketing Solutions`},
-        {i: 'Customer Segments',    x: 8, y: 0, w: 2, h: 8, text: `* Internet Users\n* Advertisers and Marketers\n* Developers\n* Recruiters`},
-        {i: 'Channels',             x: 6, y: 4, w: 2, h: 4, text: `* LinkedIn Website\n* Field Sales`},
-        {i: 'Key Resources',        x: 2, y: 4, w: 2, h: 4, text: `* LinkedIn Platform`},
-        {i: 'Cost',                 x: 0, y: 8, w: 5, h: 4, text: `* Web Hosting Costs\n* Marketing & Sales\n* Product Dvpt\n* General Administrative`},
-        {i: 'Revenue Streams',      x: 5, y: 8, w: 5, h: 4, text: `* Free Offerings & Premium Subscriptions\n* Hiring Solutions\n* Marketing Solutions`},
-        ]
+          layout:  this.getInitialLayout(),
+          texts: this.getInitialTexts()
+        
         }
 
   constructor(props){
@@ -34,60 +26,107 @@ export default class App extends React.Component {
     this.handleVersionChange = this.handleVersionChange.bind(this);
     this.saveToCookies = this.saveToCookies.bind(this);
     this.printDocument = this.printDocument.bind(this);
+    this.resetLayout = this.resetLayout.bind(this);
+    this.handleLayoutChange = this.handleLayoutChange.bind(this);
   }
   componentDidMount(){
-    try{
-    let bmCookie = Cookies.get('business_model')
-    if(bmCookie){
-      bmCookie = JSON.parse(bmCookie)
-      if(bmCookie.length> 0){
 
+    try{
+      let bmCookie = Cookies.get('business_model')
+      if(bmCookie){
+        bmCookie = JSON.parse(bmCookie)
+        if(bmCookie.layout){
+          console.log("setting layout")
+          this.updateLayout(bmCookie.layout)
+          this.setState({texts: bmCookie.texts})
+        }
       }
-      this.setState({layout:bmCookie})
-    }
     }catch(error){
       console.error("could not parse cookie")
     }
   }
 
+  updateLayout(newLayout){ //nasty fix
+    this.setState({layout:[]}, function(){
+      this.setState({layout:newLayout})
+    })
+  }
+
+  getInitialLayout(){
+    return [{i: 'Key Partners',        x: 0, y: 0, w: 2, h: 8},
+    {i: 'Key Activities',       x: 2, y: 0, w: 2, h: 4},
+    {i: 'Value Propositions',   x: 4, y: 0, w: 2, h: 8 },
+    {i: 'Customer Relations',   x: 6, y: 0, w: 2, h: 4},
+    {i: 'Customer Segments',    x: 8, y: 0, w: 2, h: 8},
+    {i: 'Channels',             x: 6, y: 4, w: 2, h: 4},
+    {i: 'Key Resources',        x: 2, y: 4, w: 2, h: 4},
+    {i: 'Cost',                 x: 0, y: 8, w: 5, h: 4},
+    {i: 'Revenue Streams',      x: 5, y: 8, w: 5, h: 4},
+    ]
+  }
+  getInitialTexts(){
+    return {
+      'Key Partners':  `* Equinix (for data Center facilities)\n* Content Providers`,
+      'Key Activities':      `* Plateform Dvpt`,
+      'Value Propositions':  `* Manage Professionnal identity\n* Build Professionnal network\n* Identify & Reach the Right Talent\n* Reach the target Audience\n* Access to LinkedIn data base content via API's widgets`,
+      'Customer Relations':  `* Internet Users\n* * Recruiters\n* * Advertisers & Marketers\n* Developers\n* Free offerings & Premium Subscriptions\n* Hiring Solutions\n* Marketing Solutions`,
+      'Customer Segments':   `* Internet Users\n* Advertisers and Marketers\n* Developers\n* Recruiters`,
+      'Channels':            `* LinkedIn Website\n* Field Sales`,
+      'Key Resources':       `* LinkedIn Platform`,
+      'Cost':                `* Web Hosting Costs\n* Marketing & Sales\n* Product Dvpt\n* General Administrative`,
+      'Revenue Streams':     `* Free Offerings & Premium Subscriptions\n* Hiring Solutions\n* Marketing Solutions`
+          }
+  }
+
   handleTextChange(value, index){
-    let layout = this.state.layout
-    layout[index].text = value
-    this.setState({layout: layout})
+    let texts = this.state.texts
+    texts[index] = value
+    this.setState({texts: texts})
+  }
+
+  handleLayoutChange(newLayout){
+    this.setState({layout:newLayout})
   }
 
   printDocument() {
-    this.setState({edit:false})
-    let items = document.getElementsByClassName('react-grid-item')
-    for(var i=0;i<items.length;i++){
-      if(items[i].style.transform.length > 10){
-        var transform = items[i].style.transform.substr(10)
-        items[i].style.left = transform.substring(0, transform.indexOf(','))
-        console.log(transform.substring(0, transform.indexOf(',')))
-        items[i].style.top = transform.substring(transform.indexOf(',')+2, transform.length-1)
-        console.log(transform.substring(transform.indexOf(',')+2, transform.length-1))
-        items[i].style.transform = ""  
+    this.setState({edit:false}, function(){
+      let items = document.getElementsByClassName('react-grid-item')
+      for(var i=0;i<items.length;i++){
+        if(items[i].style.transform.length > 10){
+          var transform = items[i].style.transform.substr(10)
+          items[i].style.left = transform.substring(0, transform.indexOf(','))
+          console.log(transform.substring(0, transform.indexOf(',')))
+          items[i].style.top = transform.substring(transform.indexOf(',')+2, transform.length-1)
+          console.log(transform.substring(transform.indexOf(',')+2, transform.length-1))
+          items[i].style.transform = ""  
+        }
       }
-    }
-
-    html2canvas(document.getElementById('divToPrint'), {
-      width: window.innerWidth,
-      height: window.innerHeight
-    }).then((canvas) => {
-        const imgData = canvas.toDataURL('image/png');
-        const pdf = new jsPDF("l", "mm", "a4");
-        var width = pdf.internal.pageSize.getWidth();
-        var height = pdf.internal.pageSize.getHeight();
-
-        pdf.addImage(imgData, 'JPEG', 0, 0, width, height);
-        pdf.save("business_model.pdf");
-      })
-    ;
+  
+      html2canvas(document.getElementById('divToPrint'), {
+        width: window.innerWidth,
+        height: window.innerHeight
+      }).then((canvas) => {
+          const imgData = canvas.toDataURL('image/png');
+          const pdf = new jsPDF("l", "mm", "a4");
+          var width = pdf.internal.pageSize.getWidth();
+          var height = pdf.internal.pageSize.getHeight();
+  
+          pdf.addImage(imgData, 'JPEG', 0, 0, width, height);
+          pdf.save("business_model.pdf");
+        })
+    })
   }
   saveToCookies(){
-   
-    Cookies.set('business_model', this.state.layout)
+    this.setState({edit:false})
+
+    Cookies.set('business_model', {layout:this.state.layout, texts:this.state.texts})
     this.showToast("model saved")
+  }
+  resetLayout(){
+    this.updateLayout(this.getInitialLayout())
+    this.setState({texts:this.getInitialTexts()})
+    this.showToast("model reset")
+    
   }
   toggleChange = () => {
     this.setState({
@@ -133,6 +172,7 @@ export default class App extends React.Component {
       <div className="main-app" >
         <div className="topbar">
             <h4>Business Model Canvas</h4>
+            <button className="generate" onClick={this.resetLayout}>Reset</button>
             <button className="generate" onClick={this.printDocument}>Save as PDF</button>
             <button className="cookify" onClick={this.saveToCookies}>Save to Cookies</button>
             <span>Edit</span>
@@ -166,7 +206,7 @@ export default class App extends React.Component {
                 :""}
             </div>)
             :""}
-          <BMGrid style={{position:'relative'}} textChange={(value, index)=>this.handleTextChange(value, index)} layout={this.state.layout} edit={this.state.edit} ></BMGrid>
+          <BMGrid style={{position:'relative'}} textChange={(value, index)=>this.handleTextChange(value, index)} texts={this.state.texts} changeLayout={(layout)=>this.handleLayoutChange(layout)} layout={this.state.layout} edit={this.state.edit} ></BMGrid>
         </div>
       
         <Toast
--- src/BMGrid.js
+++ src/BMGrid.js
@@ -7,26 +7,38 @@ import BMBlock from './BMBlock';
 
 class BMGrid extends React.Component {
 
-    state = {width:1200}
+    state = {width:1200, layout:[]}
     
     constructor(props){
         super(props)
         this.handleTextChange = this.handleTextChange.bind(this)
+        this.updateDimensions = this.updateDimensions.bind(this);
     }    
     
     componentDidMount(){
-        this.setState({width:window.innerWidth, height:window.innerHeight*0.06})
+        this.setState({width:window.innerWidth, height:window.innerHeight*0.06, layout:this.props.layout, texts:this.props.texts})
+        window.addEventListener("resize", this.updateDimensions);
     }
 
+    updateDimensions(){
+        let layout = this.state.layout
+        this.setState({width:window.innerWidth, height:window.innerHeight*0.06, layout:this.props.layout})
+        console.log('resized')
+    }
 
     handleTextChange(value, index){
         this.props.textChange(value, index)
+    }
+
+    handleChartResize = (layout) => {
+        console.log('this is the layout', layout);
+        this.props.changeLayout(layout)
     }
     
     getBlocks(){
         return this.props.layout.map((block, index) => 
         (<div  className = "bm-block" key={block.i}>
-            <BMBlock edit={this.props.edit} textChange={(value)=>this.handleTextChange(value, index)} text={block.text} name={block.i} ></BMBlock>
+            <BMBlock edit={this.props.edit} textChange={(value)=>this.handleTextChange(value, block.i)} text={this.props.texts[block.i]} name={block.i} ></BMBlock>
         </div>))
     }
 
@@ -36,6 +48,7 @@ class BMGrid extends React.Component {
             className="layout bl-layout" 
             layout={this.props.layout} cols={10}  
             width={this.state.width}
+            onLayoutChange={this.handleChartResize}
             rowHeight={this.state.height}>
             {this.getBlocks()}
         </GridLayout>
