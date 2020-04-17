93769aa4407898556c8c13e3c6bc7e08806f20df
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-17 13:34:03 +0000

    Deploy emrhzc/businesss-model-canvas to github.com/emrhzc/businesss-model-canvas.git:gh-pages

---
+++ precache-manifest.809ead643c2a7285ac28f53c3e8c6d2b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "102ec56fe78e9c9c9119c7d6569e4b00",
+    "url": "/businesss-model-canvas/index.html"
+  },
+  {
+    "revision": "5fb4197b0f2240100428",
+    "url": "/businesss-model-canvas/static/css/2.ad542f85.chunk.css"
+  },
+  {
+    "revision": "fe7fd0348def171e6cea",
+    "url": "/businesss-model-canvas/static/css/main.5297f67a.chunk.css"
+  },
+  {
+    "revision": "5fb4197b0f2240100428",
+    "url": "/businesss-model-canvas/static/js/2.22ba3ada.chunk.js"
+  },
+  {
+    "revision": "769acb7d795e27227e3a91c842258ff7",
+    "url": "/businesss-model-canvas/static/js/2.22ba3ada.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe7fd0348def171e6cea",
+    "url": "/businesss-model-canvas/static/js/main.1e8c4e96.chunk.js"
+  },
+  {
+    "revision": "ec7d2550f26ac96183b9",
+    "url": "/businesss-model-canvas/static/js/runtime-main.b49f81c0.js"
+  },
+  {
+    "revision": "2c2e3fc7b3db8c5ab923e552344715aa",
+    "url": "/businesss-model-canvas/static/media/success.2c2e3fc7.svg"
+  }
+]);
